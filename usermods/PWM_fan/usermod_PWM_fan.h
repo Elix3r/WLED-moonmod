8320d3c9d8bea44f73136280be1ed3b62c255311
Elix3r <rmartinez370@gmail.com>
2024-05-08 19:07:21 -0700

    Update usermod_PWM_fan.h

--- usermods/PWM_fan/usermod_PWM_fan.h
+++ usermods/PWM_fan/usermod_PWM_fan.h
@@ -10,7 +10,6 @@
 #define PWMFAN_MQTT_TOPIC "wled/PWMFan"
 #endif
 
-// Define tacho and PWM pins
 #ifndef TACHO_PIN
 #define TACHO_PIN -1
 #endif
@@ -19,10 +18,8 @@
 #define PWM_PIN -1
 #endif
 
-// Tacho counter
 static volatile unsigned long counter_rpm = 0;
 
-// Interrupt counting every rotation of the fan
 static void IRAM_ATTR rpm_fan() {
   counter_rpm++;
 }
@@ -37,7 +34,7 @@ class PWMFanUsermod : public Usermod {
     uint8_t pwmChannel = 255;
     #endif
     bool lockFan = false;
-    char pwmFanMqttTopic[64];
+    uint8_t pwmValuePct = 0; // Ensure this is declared at class level
 
     #ifdef USERMOD_DALLASTEMPERATURE
     UsermodTemperature* tempUM;
@@ -50,36 +47,16 @@ class PWMFanUsermod : public Usermod {
     uint8_t tachoUpdateSec = 30;
     float targetTemperature = 35.0;
     uint8_t minPWMValuePct = 0;
-    uint8_t maxPWMValuePct = 100;
     uint8_t numberOfInterrupsInOneSingleRotation = 2;
 
-    // Constants for PWM control
-    static const uint8_t _pwmMaxValue = 255;
-    static const uint8_t _pwmMaxStepCount = 7;
-    float _pwmTempStepSize = 0.5f;
-
-    // Static member declarations
-    static const char _name[];
-    static const char _enabled[];
-    static const char _tachoPin[];
-    static const char _pwmPin[];
-    static const char _temperature[];
-    static const char _tachoUpdateSec[];
-    static const char _minPWMValuePct[];
-    static const char _maxPWMValuePct[];
-    static const char _IRQperRotation[];
-    static const char _speed[];
-    static const char _lock[];
-
     void initTacho(void) {
-      if (tachoPin < 0 || !pinManager.allocatePin(tachoPin, false, PinOwner::UM_Unspecified)) {
+      if (tachoPin < 0 || !pinManager.allocatePin(tachoPin, false, PinOwner::UM_Unspecified)){
         tachoPin = -1;
         return;
       }
       pinMode(tachoPin, INPUT);
       digitalWrite(tachoPin, HIGH);
       attachInterrupt(digitalPinToInterrupt(tachoPin), rpm_fan, FALLING);
-      DEBUG_PRINTLN(F("Tacho successfully initialized."));
     }
 
     void deinitTacho(void) {
@@ -89,29 +66,12 @@ class PWMFanUsermod : public Usermod {
       tachoPin = -1;
     }
 
-    void updateTacho(void) {
-      msLastTachoMeasurement = millis();
-      if (tachoPin < 0) return;
-
-      detachInterrupt(digitalPinToInterrupt(tachoPin));
-      last_rpm = (counter_rpm * 60) / numberOfInterrupsInOneSingleRotation;
-      last_rpm /= tachoUpdateSec;
-      counter_rpm = 0;
-      attachInterrupt(digitalPinToInterrupt(tachoPin), rpm_fan, FALLING);
-
-      if (WLED_MQTT_CONNECTED) {
-        char buff[16];
-        sprintf(buff, "%u RPM", last_rpm);
-        mqtt->publish(pwmFanMqttTopic, 0, false, buff);
-      }
-    }
-
     void initPWMfan(void) {
       if (pwmPin < 0 || !pinManager.allocatePin(pwmPin, true, PinOwner::UM_Unspecified)) {
         enabled = false;
+        pwmPin = -1;
         return;
       }
-
       #ifdef ESP8266
       analogWriteRange(255);
       analogWriteFreq(WLED_PWM_FREQ);
@@ -124,59 +84,58 @@ class PWMFanUsermod : public Usermod {
       ledcSetup(pwmChannel, 25000, 8);
       ledcAttachPin(pwmPin, pwmChannel);
       #endif
-      DEBUG_PRINTLN(F("Fan PWM successfully initialized."));
     }
 
-    void updateFanSpeed(uint8_t pwmValue) {
-      if (!enabled || pwmPin < 0) return;
+    void deinitPWMfan(void) {
+      if (pwmPin < 0) return;
+      #ifdef ARDUINO_ARCH_ESP32
+      ledcDetachPin(pwmPin);
+      ledcStop(pwmChannel, 0); // Stop with a duty cycle of 0
+      #endif
+      pinManager.deallocatePin(pwmPin, PinOwner::UM_Unspecified);
+      pwmPin = -1;
+    }
 
+    void updateTacho(void) {
+      msLastTachoMeasurement = millis();
+      if (tachoPin < 0) return;
+      detachInterrupt(digitalPinToInterrupt(tachoPin));
+      last_rpm = (counter_rpm * 60) / numberOfInterrupsInOneSingleRotation;
+      last_rpm /= tachoUpdateSec;
+      counter_rpm = 0;
+      attachInterrupt(digitalPinToInterrupt(tachoPin), rpm_fan, FALLING);
+    }
+
+    void updateFanSpeed(uint8_t pwmValue){
+      if (!enabled || pwmPin < 0) return;
       #ifdef ESP8266
       analogWrite(pwmPin, pwmValue);
       #else
       ledcWrite(pwmChannel, pwmValue);
       #endif
-
-      if (WLED_MQTT_CONNECTED) {
-        char buff[16];
-        sprintf(buff, "%d%%", pwmValue);
-        mqtt->publish(pwmFanMqttTopic, 0, false, buff);
-      }
     }
 
     float getActualTemperature(void) {
-      #if defined(USERMOD_DALLASTEMPERATURE) || defined(USERMOD_SHT)
       if (tempUM != nullptr)
         return tempUM->getTemperatureC();
-      #endif
       return -127.0f;
     }
 
     void setFanPWMbasedOnTemperature(void) {
       float temp = getActualTemperature();
-      int pwmStepSize = ((maxPWMValuePct - minPWMValuePct) * _pwmMaxValue) / (_pwmMaxStepCount * 100);
-      int pwmStep = calculatePwmStep(temp - targetTemperature);
-      int pwmMinimumValue = (minPWMValuePct * _pwmMaxValue) / 100;
-      updateFanSpeed(pwmMinimumValue + pwmStep * pwmStepSize);
-    }
-
-    uint8_t calculatePwmStep(float diffTemp) {
-      if ((diffTemp == NAN) || (diffTemp <= -100.0)) {
-        return _pwmMaxStepCount;
+      float difftemp = temp - targetTemperature;
+      int newPWMvalue = 255;
+      if (difftemp <= 0.0) {
+        newPWMvalue = minPWMValuePct * newPWMvalue / 100;
+      } else {
+        int steps = (int)(difftemp / 0.5f) + 1; // Assuming a step of 0.5 degrees
+        newPWMvalue = minPWMValuePct * newPWMvalue / 100 + steps * ((100 - minPWMValuePct) * newPWMvalue / 700); // Assuming 7 steps max
       }
-      if (diffTemp <= 0) {
-        return 0;
-      }
-      return (uint8_t) min((int)_pwmMaxStepCount, (int)(diffTemp / _pwmTempStepSize) + 1);
+      updateFanSpeed(newPWMvalue);
     }
 
   public:
-    void setup() override {
-      #ifdef USERMOD_DALLASTEMPERATURE
-      tempUM = (UsermodTemperature*) usermods.lookup(USERMOD_ID_TEMPERATURE);
-      #elif defined(USERMOD_SHT)
-      tempUM = (ShtUsermod*) usermods.lookup(USERMOD_ID_SHT);
-      #endif
-      sprintf(pwmFanMqttTopic, "%s/status", PWMFAN_MQTT_TOPIC);
+    void setup() {
       initTacho();
       initPWMfan();
       updateFanSpeed((minPWMValuePct * 255) / 100); // Initial fan speed
@@ -185,178 +144,63 @@ class PWMFanUsermod : public Usermod {
 
     void loop() {
       if (!enabled || strip.isUpdating()) return;
-
       unsigned long now = millis();
       if ((now - msLastTachoMeasurement) < (tachoUpdateSec * 1000)) return;
-
       updateTacho();
       if (!lockFan) setFanPWMbasedOnTemperature();
     }
 
-    // additional methods...
     void addToJsonInfo(JsonObject& root) {
       JsonObject user = root["u"];
       if (user.isNull()) user = root.createNestedObject("u");
-
-      JsonArray infoArr = user.createNestedArray(FPSTR(_name));
-      String uiDomString = F("<button class=\"btn btn-xs\" onclick=\"requestJson({'");
-      uiDomString += FPSTR(_name);
-      uiDomString += F("':{'");
-      uiDomString += FPSTR(_enabled);
-      uiDomString += F("':");
+      JsonArray infoArr = user.createNestedArray("PWM-fan");
+      String uiDomString = "<button class=\"btn btn-xs\" onclick=\"requestJson({'PWM-fan':{'enabled':";
       uiDomString += enabled ? "false" : "true";
-      uiDomString += F("}});\"><i class=\"icons ");
+      uiDomString += "}});\"><i class=\"icons ";
       uiDomString += enabled ? "on" : "off";
-      uiDomString += F("\">&#xe08f;</i></button>");
+      uiDomString += "\">&#xe08f;</i></button>";
       infoArr.add(uiDomString);
 
       if (enabled) {
-        JsonArray infoArr = user.createNestedArray(F("Manual"));
-        String uiDomString = F("<div class=\"slider\"><div class=\"sliderwrap il\"><input class=\"noslide\" onchange=\"requestJson({'");
-        uiDomString += FPSTR(_name);
-        uiDomString += F("':{'");
-        uiDomString += FPSTR(_speed);
-        uiDomString += F("':parseInt(this.value)}});\" oninput=\"updateTrail(this);\" max=100 min=0 type=\"range\" value=");
-        uiDomString += pwmValuePct;
-        uiDomString += F(" /><div class=\"sliderdisplay\"></div></div></div>"); //<output class=\"sliderbubble\"></output>
+        JsonArray infoArr = user.createNestedArray("Manual");
+        String uiDomString = "<div class=\"slider\"><div class=\"sliderwrap il\"><input class=\"noslide\" onchange=\"requestJson({'PWM-fan':{'speed':parseInt(this.value)}});\" oninput=\"updateTrail(this);\" max=100 min=0 type=\"range\" value=";
+        uiDomString += String(pwmValuePct);
+        uiDomString += " /><div class=\"sliderdisplay\"></div></div></div>";
         infoArr.add(uiDomString);
 
-        JsonArray data = user.createNestedArray(F("Speed"));
+        JsonArray data = user.createNestedArray("Speed");
         if (tachoPin >= 0) {
           data.add(last_rpm);
-          data.add(F("rpm"));
+          data.add("rpm");
         } else {
-          if (lockFan) data.add(F("locked"));
-          else         data.add(F("auto"));
+          if (lockFan) data.add("locked");
+          else         data.add("auto");
         }
       }
     }
 
-    /*
-     * addToJsonState() can be used to add custom entries to the /json/state part of the JSON API (state object).
-     * Values in the state object may be modified by connected clients
-     */
-    //void addToJsonState(JsonObject& root) {
-    //}
-
-    /*
-     * readFromJsonState() can be used to receive data clients send to the /json/state part of the JSON API (state object).
-     * Values in the state object may be modified by connected clients
-     */
     void readFromJsonState(JsonObject& root) {
-      if (!initDone) return;  // prevent crash on boot applyPreset()
-      JsonObject usermod = root[FPSTR(_name)];
+      if (!initDone) return; // Prevent crash on boot applyPreset()
+      JsonObject usermod = root["PWM-fan"];
       if (!usermod.isNull()) {
-        if (usermod[FPSTR(_enabled)].is<bool>()) {
-          enabled = usermod[FPSTR(_enabled)].as<bool>();
+        if (usermod["enabled"].is<bool>()) {
+          enabled = usermod["enabled"].as<bool>();
           if (!enabled) updateFanSpeed(0);
         }
-        if (enabled && !usermod[FPSTR(_speed)].isNull() && usermod[FPSTR(_speed)].is<int>()) {
-          pwmValuePct = usermod[FPSTR(_speed)].as<int>();
-          updateFanSpeed((constrain(pwmValuePct,0,100) * 255) / 100);
+        if (enabled && !usermod["speed"].isNull() && usermod["speed"].is<int>()) {
+          pwmValuePct = usermod["speed"].as<int>();
+          updateFanSpeed((constrain(pwmValuePct, 0, 100) * 255) / 100);
           if (pwmValuePct) lockFan = true;
         }
-        if (enabled && !usermod[FPSTR(_lock)].isNull() && usermod[FPSTR(_lock)].is<bool>()) {
-          lockFan = usermod[FPSTR(_lock)].as<bool>();
+        if (enabled && !usermod["lock"].isNull() && usermod["lock"].is<bool>()) {
+          lockFan = usermod["lock"].as<bool>();
         }
       }
     }
 
-    void appendConfigData() {
-      oappend(SET_F("addHB('PWM-fan');"));
-    }
-
-    /*
-     * addToConfig() can be used to add custom persistent settings to the cfg.json file in the "um" (usermod) object.
-     * It will be called by WLED when settings are actually saved (for example, LED settings are saved)
-     * If you want to force saving the current state, use serializeConfig() in your loop().
-     * 
-     * CAUTION: serializeConfig() will initiate a filesystem write operation.
-     * It might cause the LEDs to stutter and will cause flash wear if called too often.
-     * Use it sparingly and always in the loop, never in network callbacks!
-     * 
-     * addToConfig() will also not yet add your setting to one of the settings pages automatically.
-     * To make that work you still have to add the setting to the HTML, xml.cpp and set.cpp manually.
-     * 
-     * I highly recommend checking out the basics of ArduinoJson serialization and deserialization in order to use custom settings!
-     */
-    void addToConfig(JsonObject& root) {
-      JsonObject top = root.createNestedObject(FPSTR(_name)); // usermodname
-      top[FPSTR(_enabled)]        = enabled;
-      top[FPSTR(_pwmPin)]         = pwmPin;
-      top[FPSTR(_tachoPin)]       = tachoPin;
-      top[FPSTR(_tachoUpdateSec)] = tachoUpdateSec;
-      top[FPSTR(_temperature)]    = targetTemperature;
-      top[FPSTR(_minPWMValuePct)] = minPWMValuePct;
-      top[FPSTR(_IRQperRotation)] = numberOfInterrupsInOneSingleRotation;
-      DEBUG_PRINTLN(F("Autosave config saved."));
-    }
-
-    /*
-     * readFromConfig() can be used to read back the custom settings you added with addToConfig().
-     * This is called by WLED when settings are loaded (currently this only happens once immediately after boot)
-     * 
-     * readFromConfig() is called BEFORE setup(). This means you can use your persistent values in setup() (e.g. pin assignments, buffer sizes),
-     * but also that if you want to write persistent values to a dynamic buffer, you'd need to allocate it here instead of in setup.
-     * If you don't know what that is, don't fret. It most likely doesn't affect your use case :)
-     * 
-     * The function should return true if configuration was successfully loaded or false if there was no configuration.
-     */
-    bool readFromConfig(JsonObject& root) {
-      int8_t newTachoPin = tachoPin;
-      int8_t newPwmPin   = pwmPin;
-
-      JsonObject top = root[FPSTR(_name)];
-      DEBUG_PRINT(FPSTR(_name));
-      if (top.isNull()) {
-        DEBUG_PRINTLN(F(": No config found. (Using defaults.)"));
-        return false;
-      }
-
-      enabled           = top[FPSTR(_enabled)] | enabled;
-      newTachoPin       = top[FPSTR(_tachoPin)] | newTachoPin;
-      newPwmPin         = top[FPSTR(_pwmPin)] | newPwmPin;
-      tachoUpdateSec    = top[FPSTR(_tachoUpdateSec)] | tachoUpdateSec;
-      tachoUpdateSec    = (uint8_t) max(1,(int)tachoUpdateSec); // bounds checking
-      targetTemperature = top[FPSTR(_temperature)] | targetTemperature;
-      minPWMValuePct    = top[FPSTR(_minPWMValuePct)] | minPWMValuePct;
-      minPWMValuePct    = (uint8_t) min(100,max(0,(int)minPWMValuePct)); // bounds checking
-      numberOfInterrupsInOneSingleRotation = top[FPSTR(_IRQperRotation)] | numberOfInterrupsInOneSingleRotation;
-      numberOfInterrupsInOneSingleRotation = (uint8_t) max(1,(int)numberOfInterrupsInOneSingleRotation); // bounds checking
-
-      if (!initDone) {
-        // first run: reading from cfg.json
-        tachoPin = newTachoPin;
-        pwmPin   = newPwmPin;
-        DEBUG_PRINTLN(F(" config loaded."));
-      } else {
-        DEBUG_PRINTLN(F(" config (re)loaded."));
-        // changing paramters from settings page
-        if (tachoPin != newTachoPin || pwmPin != newPwmPin) {
-          DEBUG_PRINTLN(F("Re-init pins."));
-          // deallocate pin and release interrupts
-          deinitTacho();
-          deinitPWMfan();
-          tachoPin = newTachoPin;
-          pwmPin   = newPwmPin;
-          // initialise
-          setup();
-        }
-      }
-
-      // use "return !top["newestParameter"].isNull();" when updating Usermod with new features
-      return !top[FPSTR(_IRQperRotation)].isNull();
-  }
-
-    /*
-     * getId() allows you to optionally give your V2 usermod an unique ID (please define it in const.h!).
-     * This could be used in the future for the system to determine whether your usermod is installed.
-     */
     uint16_t getId() {
-        return USERMOD_ID_PWM_FAN;
+      return USERMOD_ID_PWM_FAN;
     }
-};
-
 };
 
 // Static member initialization
